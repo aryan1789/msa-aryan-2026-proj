6673d4865d44c4131630ab8f76886b339986473f
Aryan Shah <qgm3818@aut.ac.nz>
2026-07-08 11:59:27 +1200

    Add unique index on user email

--- backend/msa-aryan-2026-proj.Api/Data/AppDbContext.cs
+++ backend/msa-aryan-2026-proj.Api/Data/AppDbContext.cs
@@ -10,4 +10,13 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(user => user.Email)
+            .IsUnique();
+    }
 }
--- backend/msa-aryan-2026-proj.Api/Migrations/20260707222505_AddUniqueEmailIndex.Designer.cs
+++ backend/msa-aryan-2026-proj.Api/Migrations/20260707222505_AddUniqueEmailIndex.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using msa_aryan_2026_proj.Api.Data;
@@ -11,9 +12,11 @@ using msa_aryan_2026_proj.Api.Data;
 namespace msa_aryan_2026_proj.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260707222505_AddUniqueEmailIndex")]
+    partial class AddUniqueEmailIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
